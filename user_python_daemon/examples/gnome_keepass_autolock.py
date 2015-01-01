ffde12ac124319f623d723896842ae7584bbf503
icasdri <icasdri@gmail.com>
2014-12-31 22:11:52 -0500

    Initial implementation of gnome_keepass_autolock example.

--- user_python_daemon/examples/gnome_keepass_autolock.py
+++ user_python_daemon/examples/gnome_keepass_autolock.py
@@ -1,2 +1,22 @@
 # Copyright 2014 icasdri
 __author__ = 'icasdri'
+
+from dbus import SessionBus
+from subprocess import Popen
+from sys import argv
+
+path_to_mono = "/usr/bin/mono"
+path_to_keepass = "/usr/share/keepass/KeePass.exe"
+
+def _handle_signal(active):
+    if active:
+        Popen([path_to_mono, path_to_keepass, "--lock-all"])
+
+def entry_point(options=None):
+    if len(options) == 2:
+        global path_to_mono, path_to_keepass
+        path_to_mono, path_to_keepass = options
+    SessionBus().add_signal_receiver(_handle_signal, "ActiveChanged", "org.gnome.ScreenSaver")
+
+def main(options=None):
+    entry_point(options)
