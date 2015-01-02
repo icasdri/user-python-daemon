072e852f84a0f7de63ef249114588a4b0defa529
icasdri <icasdri@gmail.com>
2015-01-02 17:26:11 -0500

    Added impementation of thunderbird notification example, using libnotify

--- user_python_daemon/examples/thunderbird_notifications.py
+++ user_python_daemon/examples/thunderbird_notifications.py
@@ -1,9 +1,52 @@
+#!/usr/bin/env python3
 # Copyright 2014 icasdri
-#
-# This module is meant to be used with the Thunderbird email client, a freedesktop notifications compliant desktop,
-# and the Thunderbird Gnome Integration plugin. It's goal is to provide a single bus name for notifications fired
-# from this plugin, allowing desktops with a "notification drawer" to display the notifications as originating from
-# a single application.
-#
-#
 __author__ = 'icasdri'
+
+import dbus
+import dbus.service
+from gi.repository import Notify
+
+MY_BUS_NAME = "org.icasdri.thunderbirdnotifications"
+MY_INTERFACE = MY_BUS_NAME
+MY_PATH = "/org/icasdri/thunderbirdnotifications"
+
+
+class ThunderbirdNotifications(dbus.service.Object):
+    def __init__(self):
+        bus_name = dbus.service.BusName(MY_BUS_NAME, bus=dbus.SessionBus())
+        dbus.service.Object.__init__(self, bus_name, MY_PATH)
+
+        Notify.init("thunderbirdnotifications")
+
+    @dbus.service.method(dbus_interface=MY_INTERFACE)
+    def ProxyNotify(self, summary, body, icon="mail-mark-unread"):
+        notification = Notify.Notification.new(summary, body, icon)
+        notification.set_category("email.arrived")
+        notification.show()
+
+def entry_point(options=None):
+    ThunderbirdNotifications()
+
+
+# This module is designed to mimick 'notify-send' if run directly -- contacting an
+# already running daemonized version of this module (through user-python-daemon) and
+# proxying the notification.
+def main():
+    import argparse
+    a_parser = argparse.ArgumentParser("Thunderbird Notifications Proxy (part of user-python-daemon)",
+                                       "Use like notify-send.")
+    a_parser.add_argument("summary", type=str)
+    a_parser.add_argument("body", type=str)
+    a_parser.add_argument("--icon", type=str, default="mail-mark-unread")
+    args = a_parser.parse_args()
+    if MY_BUS_NAME in dbus.SessionBus().list_names():
+        dbus.Interface(dbus.SessionBus().get_object(MY_BUS_NAME, MY_PATH), MY_INTERFACE)\
+            .ProxyNotify(args.summary, args.body, args.icon)
+        print("Notification sent to proxy daemon.")
+    else:
+        print("Daemonized version of module (accessible via entry_point() function, "
+              "see user-python-daemon) is not running! Exitting.")
+
+
+if __name__ == "__main__":
+    main()
